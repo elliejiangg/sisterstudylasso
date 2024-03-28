d419c01b1b49aa6f0d0c2cf1bf5197cd6ee066ed
Ellie Jiang <elliejiang8@gmail.com>
2024-03-27 21:13:32 -0400

    sisterstudy commit

---
+++ code.R
@@ -0,0 +1,58 @@
+set.seed(100)
+#training / testing data
+size = floor(0.8 * nrow(chemicals))
+
+train_ind = sample(seq_len(nrow(chemicals)), size = size)
+
+train = chemicals[train_ind,]
+x_train = train[,1:189]
+x_train = sapply(x_train, as.numeric)
+y_train = train[,190]
+#ctrl = 0, case = 1
+y_train[y_train == "case"] <- "1"
+y_train[y_train == 'ctrl']<- "0"
+y_train = sapply(y_train, as.numeric)
+
+test = chemicals[-train_ind,]
+x_test = test[,1:189]
+x_test = sapply(x_test, as.numeric)
+y_test = test[,190]
+y_test[y_test == "case"] <- "1"
+y_test[y_test == 'ctrl']<- "0"
+y_test = sapply(y_test, as.numeric)
+
+install.packages("glmnet")
+library('glmnet')
+
+
+#preparing data sets for cross validation
+chemnum = sapply(chemicals, as.numeric)
+ynum = chemicals[,190]
+ynum[ynum == "case"] <- "1"
+ynum[ynum == 'ctrl']<- "0"
+ynum = sapply(ynum, as.numeric)
+y = data.frame(ynum)
+new_data =  as.matrix(chemnum[,-190])
+cv_model = cv.glmnet(as.matrix(new_data), ynum,
+                     alpha = 1, nfolds = 10)
+
+#cross validation model
+plot(cv_model)
+
+optimal_model = glmnet(as.matrix(chemnum[, -190]), ynum, alpha = 1, lambda = cv_model$lambda.min)
+
+
+
+#getting the optimal lambda
+predictions = predict(optimal_model, newx = new_data, s = cv_model$lambda.min)
+
+fitt = glmnet(x_train, y_train, alpha = 1, lambda = predictions,
+             family = "binomial")
+
+plot(fitt, xvar = 'lambda', label = T )
+#feature selection
+c = coef(cv_model, s = 'lambda.min', exact = TRUE)
+inds = which(c!=0)
+variables = row.names(c)[inds]
+variables = variables [!variables %in% '(Intercept)']
+variables
