001ac78df4391434eb9c7c2e1d55fc231b89bea7
Ellie Jiang <elliejiang8@gmail.com>
2024-03-31 20:51:01 -0400

    updated code commit

--- code.R
+++ code.R
@@ -1,4 +1,4 @@
-#Libraries
+#activating appropriate libraries
 library(glmnet)
 library(readxl)
 library(data.table)
@@ -8,7 +8,7 @@ library(ggplot2)
 log2_abundance_189_chems <- read_excel("log2_abundance_189_chems.xlsx")
 chemicals <- log2_abundance_189_chems[,2:191]
 
-#training / testing data
+#setting training data
 size = floor(0.8 * nrow(chemicals))
 
 set.seed(100)
@@ -18,19 +18,23 @@ train = chemicals[train_ind,]
 x_train = train[,1:189]
 x_train = sapply(x_train, as.numeric)
 y_train = train[,190]
+
+#creating dependent variable for training data
 #ctrl = 0, case = 1
 y_train[y_train == "case"] <- "1"
 y_train[y_train == 'ctrl']<- "0"
 y_train = sapply(y_train, as.numeric)
 
+#creating testing data
 test = chemicals[-train_ind,]
 x_test = test[,1:189]
 x_test = sapply(x_test, as.numeric)
+
+#creating dependent vairable for testing data
 y_test = test[,190]
 y_test[y_test == "case"] <- "1"
 y_test[y_test == 'ctrl']<- "0"
 y_test = sapply(y_test, as.numeric)
-
 
 
 #preparing data sets for cross validation
@@ -41,6 +45,8 @@ ynum[ynum == 'ctrl']<- "0"
 ynum = sapply(ynum, as.numeric)
 y = data.frame(ynum)
 new_data =  as.matrix(chemnum[,-190])
+
+#cross validation
 cv_model = cv.glmnet(as.matrix(new_data), ynum,
                      alpha = 1, nfolds = 10)
 
@@ -58,6 +64,7 @@ fitt = glmnet(x_train, y_train, alpha = 1, lambda = predictions,
              family = "binomial")
 
 plot(fitt, xvar = 'lambda', label = T )
+
 #feature selection
 c = coef(cv_model, s = 'lambda.min', exact = TRUE)
 inds = which(c!=0)
